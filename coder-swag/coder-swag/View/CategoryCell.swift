ee3fb0b315c0761abff7ef315aa34b6048c22a35
Tom <thwoodley@gmail.com>
2020-06-25 13:13:28 +0100

    End of Session 1

---
+++ coder-swag/coder-swag/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Tom Woodley on 25/06/2020.
+//  Copyright © 2020 Tom Woodley. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
