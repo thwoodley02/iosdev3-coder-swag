98f0db17d107d2ba06a46b3b83092732afbf20c3
Tom <thwoodley@gmail.com>
2020-06-25 14:53:36 +0100

    End of Session 3

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Tom Woodley on 25/06/2020.
+//  Copyright © 2020 Tom Woodley. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
